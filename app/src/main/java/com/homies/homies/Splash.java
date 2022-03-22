8ea6d27b3be373924b1decf9a684e0da1a54a501
Víctor <victor.ramos90@gmail.com>
2022-03-22 17:41:20 +0100

    xml-scroll
    Splash function and register scroll

--- app/src/main/java/com/homies/homies/Splash.java
+++ app/src/main/java/com/homies/homies/Splash.java
@@ -2,7 +2,11 @@ package com.homies.homies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this,RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer time = new Timer();
+        time.schedule(task,3000);
     }
 }
