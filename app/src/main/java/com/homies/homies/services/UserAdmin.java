7120ad90b6d956fbb8edf7dd771ec4c4dad61bd6
antonio <antonio_ruescas@hotmail.com>
2022-04-12 23:29:49 +0200

    add changes userAdmin

--- app/src/main/java/com/homies/homies/services/UserAdmin.java
+++ app/src/main/java/com/homies/homies/services/UserAdmin.java
@@ -1,13 +1,67 @@
 package com.homies.homies.services;
 
 public class UserAdmin {
-    private Integer id;
-    private String photo;
+    private int id;
+    private byte photo;
     private String photoContentType;
     private String phone;
-    private String premium;
+    private boolean premium;
     private String birthDate;
     private String addDate;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public byte getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte photo) {
+        this.photo = photo;
+    }
+
+    public String getPhotoContentType() {
+        return photoContentType;
+    }
+
+    public void setPhotoContentType(String photoContentType) {
+        this.photoContentType = photoContentType;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isPremium() {
+        return premium;
+    }
+
+    public void setPremium(boolean premium) {
+        this.premium = premium;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(String addDate) {
+        this.addDate = addDate;
+    }
 }
