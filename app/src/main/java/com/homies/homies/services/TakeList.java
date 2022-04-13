0283fd90f951343b5ed4f703761b427fd8dc245f
antonio <antonio_ruescas@hotmail.com>
2022-04-13 12:46:11 +0200

    add takeList

---
+++ app/src/main/java/com/homies/homies/services/TakeList.java
@@ -0,0 +1,24 @@
+package com.homies.homies.services;
+
+public class TakeList {
+
+    private int id;
+    private String nameList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameList() {
+        return nameList;
+    }
+
+    public void setNameList(String nameList) {
+        this.nameList = nameList;
+    }
+}
+
