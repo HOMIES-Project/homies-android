55dec27b13c01c6b0a8434ad7df5c2d457dfd1f1
Víctor <victor.ramos90@gmail.com>
2022-04-27 19:46:59 +0200

    name group toolbar

--- app/src/main/java/com/homies/homies/groups/DetailGroupFragment.java
+++ app/src/main/java/com/homies/homies/groups/DetailGroupFragment.java
@@ -1,10 +1,16 @@
 package com.homies.homies.groups;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +19,13 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.homies.homies.R;
+import com.homies.homies.retrofit.config.NetworkConfig;
+import com.homies.homies.retrofit.model.GroupResponse;
+import com.homies.homies.retrofit.model.UserData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class DetailGroupFragment extends Fragment {
@@ -26,6 +39,9 @@ public class DetailGroupFragment extends Fragment {
         View main = inflater.inflate(R.layout.fragment_detail_group, container, false);
 
         btnEditGroup = main.findViewById(R.id.btn_editGroup);
+
+
+        groupInfo();
 
         btnEditGroup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,5 +62,38 @@ public class DetailGroupFragment extends Fragment {
 
 
         return main;
+    }
+    public void groupInfo() {
+        SharedPreferences preferences = getActivity().getSharedPreferences("MY_APP", Context.MODE_PRIVATE);
+        String retrivedToken  = preferences.getString("TOKEN",null);
+        int userId  = preferences.getInt("USER_ID",0);
+        Call<GroupResponse> groupResponseCall = NetworkConfig.getService().groupInfo("Bearer " + retrivedToken,userId);
+        groupResponseCall.enqueue(new Callback<GroupResponse>() {
+            @Override
+            public void onResponse(Call<GroupResponse> call, Response<GroupResponse> response) {
+                if (response.isSuccessful()) {
+                    GroupResponse adslist= response.body();
+
+                    String user = adslist.getGroupName();
+                    ((MenuActivity)getActivity()).getSupportActionBar().setTitle(user);
+
+
+
+
+                } else {
+                    String message = getString(R.string.error_login);
+                    Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<GroupResponse> call, Throwable t) {
+                String message = t.getLocalizedMessage();
+                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
